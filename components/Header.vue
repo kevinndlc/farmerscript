003d832c4dc86bf139fbb7e87d9798061e710bad
Kevin <kevin@nedellec.eu>
2022-01-07 11:24:47 +0100

    Added commas next to RPC list items

--- components/Header.vue
+++ components/Header.vue
@@ -7,24 +7,57 @@
           <img class="h-10 w-auto" src="~assets/img/header-logo.png" alt="" />
         </div>
         <div class="flex items-center flex-shrink-0">
-          <div v-if="userAccount && userRessources" class="hidden sm:flex items-center">
-            <img v-if="nrjToRecover > 0" class="h-6 w-6 mr-2 cursor-pointer" src="~assets/img/plus.png" alt="Plus" @click="recoverNrj">
-            <span class="text-gray-600 font-medium mr-2">{{ userRessources.energy }} / {{ userRessources.max_energy }}</span>
-            <img class="h-8 w-8 mr-2" src="~assets/img/nrj.png" alt="Energy">
+          <div
+            v-if="userAccount && userRessources"
+            class="hidden sm:flex items-center"
+          >
+            <img
+              v-if="nrjToRecover > 0"
+              class="h-6 w-6 mr-2 cursor-pointer"
+              src="~assets/img/plus.png"
+              alt="Plus"
+              @click="recoverNrj"
+            />
+            <span class="text-gray-600 font-medium mr-2"
+              >{{ userRessources.energy }} /
+              {{ userRessources.max_energy }}</span
+            >
+            <img class="h-8 w-8 mr-2" src="~assets/img/nrj.png" alt="Energy" />
           </div>
           <div class="flex items-center gap-4">
             <div v-if="!userAccount" class="hidden sm:block">
               <label for="rpc" class="text-gray-800 font-medium">RPC :</label>
-              <select id="rpc" v-model="favoriteRpc" class="bg-transparent text-gray-600">
+              <select
+                id="rpc"
+                v-model="favoriteRpc"
+                class="bg-transparent text-gray-600"
+              >
                 <option v-for="rpc in rpcList" :key="rpc">{{ rpc }}</option>
               </select>
             </div>
-            <PrimaryButton v-if="!userAccount" @click="handleLogin">Log in</PrimaryButton>
-            <PrimaryButton v-else @click="showRessources = !showRessources">{{ showRessourcesText }}</PrimaryButton>
+            <PrimaryButton v-if="!userAccount" @click="handleLogin"
+              >Log in</PrimaryButton
+            >
+            <PrimaryButton v-else @click="showRessources = !showRessources">{{
+              showRessourcesText
+            }}</PrimaryButton>
           </div>
         </div>
         <transition v-if="userAccount" name="fade">
-          <RessourcesCard v-show="showRessources && userRessources" class="absolute w-full bg-gray-100 border-2 p-4 border-primary z-50 right-0 top-20" />
+          <RessourcesCard
+            v-show="showRessources && userRessources"
+            class="
+              absolute
+              w-full
+              bg-gray-100
+              border-2
+              p-4
+              border-primary
+              z-50
+              right-0
+              top-20
+            "
+          />
         </transition>
       </div>
     </div>
@@ -43,14 +76,16 @@ export default {
         'https://api.wax.greeneosio.com',
         'https://wax.cryptolions.io',
         'https://api-wax.eosauthority.com',
-        'https://wax.greymass.com'
-        'https://wax.alohaeos.com'
-        'https://wax.waxsweden.com'
-        'https://wax.pink.com'
-        'https://wax.dapplica.com'
-        'https://wax.eosphere.com'
+        'https://wax.greymass.com',
+        'https://wax.alohaeos.com',
+        'https://wax.waxsweden.com',
+        'https://wax.pink.com',
+        'https://wax.dapplica.com',
+        'https://wax.eosphere.com',
       ],
-      favoriteRpc: localStorage.getItem('favoriteRpc') ? localStorage.getItem('favoriteRpc') : 'https://api.wax.greeneosio.com'
+      favoriteRpc: localStorage.getItem('favoriteRpc')
+        ? localStorage.getItem('favoriteRpc')
+        : 'https://api.wax.greeneosio.com',
     }
   },
   computed: {
@@ -83,30 +118,36 @@ export default {
     },
     showRessourcesText() {
       return this.showRessources ? 'Close' : this.userAccount
-    }
+    },
   },
   watch: {
     favoriteRpc(newVal) {
       localStorage.setItem('favoriteRpc', newVal)
-    }
+    },
   },
   methods: {
     async handleLogin() {
-      this.$store.dispatch('getWax', new waxjs.WaxJS({
-        rpcEndpoint: this.favoriteRpc,
-        tryAutoLogin: true
-      }))
+      this.$store.dispatch(
+        'getWax',
+        new waxjs.WaxJS({
+          rpcEndpoint: this.favoriteRpc,
+          tryAutoLogin: true,
+        })
+      )
       await this.$store.dispatch('getUserAccount')
       if (this.userAccount) {
-        this.$toast.success({
-          component: CustomNotification,
-          props: {
-            title: 'Login success',
-            message: `You're now logged as ${this.userAccount}`
+        this.$toast.success(
+          {
+            component: CustomNotification,
+            props: {
+              title: 'Login success',
+              message: `You're now logged as ${this.userAccount}`,
+            },
+          },
+          {
+            timeout: 3000,
           }
-        }, {
-          timeout: 3000
-        })
+        )
       }
     },
 
@@ -118,35 +159,42 @@ export default {
           component: CustomNotification,
           props: {
             title: 'Not enough food',
-            message: `You need ${priceInFood} FOOD to recover your energy`
-          }
+            message: `You need ${priceInFood} FOOD to recover your energy`,
+          },
         })
       } else {
         try {
-          await this.wax.api.transact({
-          actions: [{
-            account: 'farmersworld',
-            name: 'recover',
-            authorization: [{
-              actor: this.wax.userAccount,
-              permission: 'active',
-            }],
-            data: {
-              owner: this.wax.userAccount,
-              energy_recovered: this.nrjToRecover
+          await this.wax.api.transact(
+            {
+              actions: [
+                {
+                  account: 'farmersworld',
+                  name: 'recover',
+                  authorization: [
+                    {
+                      actor: this.wax.userAccount,
+                      permission: 'active',
+                    },
+                  ],
+                  data: {
+                    owner: this.wax.userAccount,
+                    energy_recovered: this.nrjToRecover,
+                  },
+                },
+              ],
             },
-          }]},
-          {
-            blocksBehind: 3,
-            expireSeconds: 30
-          })
+            {
+              blocksBehind: 3,
+              expireSeconds: 30,
+            }
+          )
         } catch (e) {
           this.$toast.error({
             component: CustomNotification,
             props: {
               title: 'Unexpected error',
-              message: e.message
-            }
+              message: e.message,
+            },
           })
           return null
         }
@@ -155,29 +203,31 @@ export default {
           component: CustomNotification,
           props: {
             title: 'Successfully recovered your energy',
-            message: `Your energy have been recovered for a total cost of ${priceInFood} FOOD`
-          }
+            message: `Your energy have been recovered for a total cost of ${priceInFood} FOOD`,
+          },
         })
 
         setTimeout(async () => {
           await this.$store.dispatch('getUserRessources')
         }, 1500)
       }
-    }
-  }
+    },
+  },
 }
 </script>
 
 <style scoped>
-  .fade-enter-active, .fade-leave-active {
-    transition: opacity .5s;
-  }
-  .fade-enter, .fade-leave-to {
-    opacity: 0;
-  }
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
 
-  select:focus > option:checked {
-    background: #98CA2D;
-    color: white;
-  }
+select:focus > option:checked {
+  background: #98ca2d;
+  color: white;
+}
 </style>
